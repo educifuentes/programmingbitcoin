69e8a971ac7410bedf4290460c5eee9c95170d47
Eduardo Cifuentes <edu.cifuentes@gmail.com>
2022-06-22 10:37:39 +0100

    feat: ch8 ex 1

--- code-ch08/op.py
+++ code-ch08/op.py
@@ -1,4 +1,5 @@
 import hashlib
+from inspect import signature
 
 from logging import getLogger
 from unittest import TestCase
@@ -708,15 +709,25 @@ def op_checkmultisig(stack, z):
     try:
         # end::source1[]
         # parse all the points
+        # refactor: use list comprehension instead of for
+        points = [S256Point.parse(sec) for sec in sec_pubkeys]
         # parse all the signatures
+        signatures = [Signature.parse(der) for der in der_signatures]
         # loop through the signatures
+        for sig in signatures:
             # if we have no more points, signatures are no good
+            if len(points) == 0:
+                return False
             # we loop until we find the point which works with this signature
+            while points:
                 # get the current point from the list of points
+                point = points.pop(0)
                 # we check if this signature goes with the current point
+                if point.verify(z, sig):
+                    break
         # the signatures are valid, so push a 1 to the stack
+        stack.append(encode_num(1))
         # tag::source1[]
-        raise NotImplementedError  # <3>
     except (ValueError, SyntaxError):
         return False
     return True
